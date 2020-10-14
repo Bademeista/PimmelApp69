d7897f97b2a8e632674505be06153ec5c53d239d
florian rück <florianrueck@web.de>
2020-10-14 19:16:03 +0200

    test1

---
+++ PimmelApp 2.0/PimmelApp 2.0/PimmelController.swift
@@ -0,0 +1,26 @@
+//
+//  PimmelController.swift
+//  PimmelApp 2.0
+//
+//  Created by Florian Rück on 14.10.20.
+//
+
+import UIKit
+
+class PimmelController : UIViewController {
+    
+    //MARK: - Variables
+    
+    private var isTouching = false
+    
+    
+    //MARK: - Outlet actions
+    
+    
+    
+    //MARK: - Coding
+    
+
+
+
+}
